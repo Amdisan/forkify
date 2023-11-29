d12189d4d9efe064ead6d01a52fa6982aa614281
Amdisan <amdisan1@gmail.com>
2023-11-29 12:52:48 +0200

    final

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
+
 export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '31bb011d-8d5c-4528-a1c7-3692e465ba2a';
+export const KEY = `${process.env.KEY}`;
 export const MODAL_CLOSE_SEC = 2.5;
